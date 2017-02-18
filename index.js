d8176b89a446c3af635a11116893643c7ac89504
Siya <siya@bonline.co.za>
2017-02-18 20:19:22 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Start Coding then...");
+
+let data = 300;
+
+const percentageCal = (total, amount) => (amount / total) *100;
+;
+
+let userper = percentageCal(60, 20);
+
+console.log(userper);
+
+//console.log(data); //
